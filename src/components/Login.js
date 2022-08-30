d10a803654d7c75bffcd77d5a370319d3975f878
Lucas G <lucasg.devmail93@gmail.com>
2022-08-30 20:11:37 -0300

    requisito 1 c/ lint -> OK

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,19 @@ class Login extends React.Component {
     return (
       <div data-testid="page-login">
         <h1>Login</h1>
+        <div>
+          <input
+            testid="login-name-input"
+            placeholder="User"
+          />
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled=""
+          >
+            Entrar
+          </button>
+        </div>
       </div>
     );
   }
